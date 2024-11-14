0e81b87c6ace87a7bc1bfaffb652b016e61e5fc9
Meledj <151031940+Meledj@users.noreply.github.com>
2024-11-14 09:19:44 +0100

    Update tree.c
    Implémentation de la fonction "display_tree" permettant d'afficher un arbre

--- tree.c
+++ tree.c
@@ -18,3 +18,20 @@ void Fill_Tree(t_tree *tree, int height, int nb_choices, int** costs, t_localisa
 {
     fill_node(tree->root, height, nb_choices, costs, loc_init, deplacements);
 }
+
+void display_tree(t_tree tree){
+    t_queue tab_file;
+    file = createEmptyQueue();
+    enqueue(&file, tree.root);
+    printf("[");
+    while( isQueueEmpty(file) == 0 ){
+        if ( file.values[file.first].nb_sons != 0){
+            for (int i = 0; i < file.values[file.first].nb_sons; i++){
+                enqueue(&file, file.values[file.first].son[i]);
+            }
+        }
+        printf(" %d :",dequeue(&file) );
+    }        
+    printf("]");
+}
+
