1cbdc56a68ed65f9e488d4ffbcc8d08617c71d54
Meledj <151031940+Meledj@users.noreply.github.com>
2024-11-21 14:25:37 +0100

    Update node.c
    Modification de la fonction "fill_node"

--- node.c
+++ node.c
@@ -45,29 +45,35 @@ t_move* remove2(t_move* deplacements, t_move dep, int TL)
 }
 
 
-void fill_node(t_node* node, int depth, int nb_choices, int** costs, t_localisation loc, t_move* deplacements)
+void fill_node(t_node* node, int depth, int nb_choices, int** costs, t_localisation loc, t_move* deplacements, int longeur, int largeur )
 {
     node->depth = depth;
     node->nbSons = nb_choices;
-    node->value = costs[loc.pos.x][loc.pos.y];
+    node->value = costs[loc.pos.y][loc.pos.x];
 
     for (int i = 0; i < nb_choices; i++)
     {
         t_move deplacement = deplacements[i];
         t_localisation new_loc = translate(loc, deplacement);
 
-        t_move* new_deplacements = remove2(deplacements, deplacement, nb_choices);
+        if (new_loc.pos.x >= 0 && new_loc.pos.x <= largeur - 1 && new_loc.pos.y >= 0 && new_loc.pos.y <= longeur - 1) {
+            t_move* new_deplacements = remove2(deplacements, deplacement, nb_choices);
 
-        if (costs[new_loc.pos.x][new_loc.pos.y] < 9999)
-        {
-            t_node* son = createNode(DEFAULT_COST, DEFAULT_NB_SONS, depth + 1);
-            fill_node(son, son->depth, nb_choices - 1, costs, new_loc, new_deplacements);
+            if (costs[new_loc.pos.y][new_loc.pos.x] < 9999)
+            {
+                t_node* son = createNode(DEFAULT_COST, nb_choices - 1, depth + 1);
 
-            node->sons[i] = son;
-            node->nbSons++;
+                son->parent = node;
+
+                fill_node(son, son->depth, nb_choices - 1, costs, new_loc, new_deplacements, longeur, largeur);
+                node->sons[i] = son;
+            }
+            free(new_deplacements);
+        }
+        else {
+            node->sons[i] = NULL;
         }
 
-        free(new_deplacements);
     }
 }
 
